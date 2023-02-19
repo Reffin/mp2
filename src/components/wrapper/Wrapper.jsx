04f49004d34eedb3c9c4f812b8dc0a281bb884db
Reffin <ryancarbonel1984@gmail.com>
2023-02-19 21:51:20 +0800

    Update Description

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -6,7 +6,7 @@ const Wrapper = () => {
     {
       cover: <i class='fa-solid fa-truck-fast'></i>,
       title: "Worldwide Delivery",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "RC ONLINE SHOP Services are proud to offer parcel delivery services for countries all around the world.",
     },
     {
       cover: <i class='fa-solid fa-id-card'></i>,
@@ -16,12 +16,12 @@ const Wrapper = () => {
     {
       cover: <i class='fa-solid fa-shield'></i>,
       title: "Shop With Confidence ",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "SHOP SAFELY AND SECURELY. Our aim is to ensure you enjoy the same level of customer satisfaction as you would if shopping in store.",
     },
     {
       cover: <i class='fa-solid fa-headset'></i>,
       title: "24/7 Support ",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "Customer support cannot be left out of the equation when choosing a  company to handle your worldwide deliveries.",
     },
   ]
   return (
